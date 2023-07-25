ea0a36e483059f9810ce92db79bdbcd8e51823e5
Fazle Rabbi Khan <fazlerabbi.frk@gmail.com>
2023-07-25 12:42:41 +0600

    grid items hardcoded to dynamic

--- app/src/main/java/com/fazlerabbikhan/utilitybills/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/fazlerabbikhan/utilitybills/presentation/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.GridLayout
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
@@ -29,6 +30,25 @@ class MainActivity : AppCompatActivity() {
         viewModel.state.observe(this) { state ->
             Log.d("ViewModelObserver", "$state")
 
+            // Clear any previous views in the gridLayout
+            binding.utilityTypesLayout.removeAllViews()
+
+            // Add the utility type cards to the grid layout dynamically
+            for (utilityType in state.utilityTypes) {
+                val utilityTypeLayout = layoutInflater.inflate(R.layout.utility_type_list_item, null) as LinearLayout
+                val textView = utilityTypeLayout.findViewById<TextView>(R.id.utility_type_name)
+                val params = GridLayout.LayoutParams()
+                params.width = 0
+                params.height = GridLayout.LayoutParams.WRAP_CONTENT
+                params.columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f)
+                utilityTypeLayout.layoutParams = params
+                textView.text = utilityType.name
+                utilityTypeLayout.setOnClickListener {
+                    viewModel.filterUtilityTypes(utilityType.name)
+                }
+                binding.utilityTypesLayout.addView(utilityTypeLayout)
+            }
+
             // Extract the list of utilities from the state
             val utilities: List<Utility> = state.filteredUtilityTypes.flatMap { utilityType -> utilityType.utilities }
 
@@ -41,18 +61,6 @@ class MainActivity : AppCompatActivity() {
                 val textView = utilityLayout.findViewById<TextView>(R.id.utility_name)
                 textView.text = utility.name
                 binding.utilitiesLayout.addView(utilityLayout)
-            }
-
-            if(state.utilityTypes.isNotEmpty()){
-                // Set OnClickListener for the 'Electricity' card
-                binding.cardElectricity.setOnClickListener {
-                    viewModel.filterUtilityTypes(state.utilityTypes[0].name)
-                }
-
-                // Set OnClickListener for the 'Electricity Prepaid' card
-                binding.cardElectricityPrepaid.setOnClickListener {
-                    viewModel.filterUtilityTypes(state.utilityTypes[1].name)
-                }
             }
         }
     }
